dea4c06cfa3467638c464f09284ff16e8010d10f
madsmansour <42993941+madsmansour@users.noreply.github.com>
2018-09-27 14:14:43 +0200

    PentagonArea

---
+++ src/Opgave4_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Opgave4_1 {
+
+    public static void main(String[] args) {
+
+        double area;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("input lenght from center to a vertex: ");
+        double r = scan.nextDouble();
+
+        double s = 2*r*Math.sin((Math.PI)/(5));
+
+        area = (5*Math.pow(s,2))/(4*(Math.tan((Math.PI)/(5))));
+
+        System.out.println("The area of the pentagon is: " + area);
+
+
+    }
+
+}
