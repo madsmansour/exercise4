c4c7565fe4d89043bad5f128095b51d3f6ec6a8a
madsmansour <42993941+madsmansour@users.noreply.github.com>
2018-09-29 19:44:07 +0200

    some of 4.4.5

---
+++ src/Chapter4Excersizes.java
@@ -0,0 +1,28 @@
+public class Chapter4Excersizes {
+
+    public static void main(String[] args) {
+
+        System.out.println("1" + 1);
+        System.out.println('1' + 1);
+        System.out.println("1" + 1 + 1);
+        System.out.println("1" + (1+1));
+        System.out.println('1' + 1 + 1);
+
+        String s1 = "Welcome";
+        String s2 = "welcome";
+
+
+        String s3 = s1.toLowerCase();
+
+        System.out.println(s3);
+
+        System.out.println(s1.toLowerCase().equals(s2));
+
+        System.out.println(s1.length());
+
+        int x = 7;
+
+        String s7 = (s1 +  s2 );
+
+        System.out.println(s7);
+}}
